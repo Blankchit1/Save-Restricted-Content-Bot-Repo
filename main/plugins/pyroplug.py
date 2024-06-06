2df2115e878b44d2eb74db091c91fa1b6fe7dbe0
Blankchit1 <bblankchit@gmail.com>
2024-06-06 20:57:45 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -384,7 +384,7 @@ async def ggn_new(userbot, client, sender, edit_id, msg_link, i, file_n):
                 progress=progress_for_pyrogram,
                 progress_args=(
                     client,
-                    "**__Unrestricting__: __[Team SPY](https://t.me/dev_gagan)__**\n ",
+                    "**__Unrestricting__: __[🕸️𓆩𝐁𝐋𝐚𝐧𝐊𝐂𝐇𝐈𝐭𓆪🧸MvMahanta](https://t.me/neetbrandmedprep)__**\n ",
                     edit,
                     time.time()
                 )
@@ -425,7 +425,7 @@ async def ggn_new(userbot, client, sender, edit_id, msg_link, i, file_n):
                     logging.info(e)
                     thumb_path = None
                 
-                caption = f"{msg.caption}\n\n__Unrestricted by **[Team SPY](https://t.me/dev_gagan)**__" if msg.caption else "__Unrestricted by **[Team SPY](https://t.me/dev_gagan)**__"
+                caption = f"{msg.caption}\n\n__Unrestricted by **[🕸️𓆩𝐁𝐋𝐚𝐧𝐊𝐂𝐇𝐈𝐭𓆪🧸MvMahanta](https://t.me/neetbrandmedprep)**__" if msg.caption else "__Unrestricted by **[🕸️𓆩𝐁𝐋𝐚𝐧𝐊𝐂𝐇𝐈𝐭𓆪🧸MvMahanta](https://t.me/neetbrandmedprep)**__"
                 await send_video_with_chat_id(client, sender, path, caption, duration, hi, wi, thumb_path, upm)
             elif str(file).split(".")[-1] in ['jpg', 'jpeg', 'png', 'webp']:
                 if file_n != '':
@@ -439,7 +439,7 @@ async def ggn_new(userbot, client, sender, edit_id, msg_link, i, file_n):
                     file = path
 
                 
-                caption = f"{msg.caption}\n\n__Unrestricted by **[Team SPY](https://t.me/dev_gagan)**__" if msg.caption else "__Unrestricted by **[Team SPY](https://t.me/dev_gagan)**__"
+                caption = f"{msg.caption}\n\n__Unrestricted by **[ 🕸️𓆩𝐁𝐋𝐚𝐧𝐊𝐂𝐇𝐈𝐭𓆪🧸MvMahanta](https://t.me/neetbrandmedprep)**__" if msg.caption else "__Unrestricted by **[🕸️𓆩𝐁𝐋𝐚𝐧𝐊𝐂𝐇𝐈𝐭𓆪🧸MvMahanta](https://t.me/neetbrandmedorep)**__"
                 await upm.edit("__Uploading photo...__")
 
                 await bot.send_file(sender, path, caption=caption)
@@ -455,7 +455,7 @@ async def ggn_new(userbot, client, sender, edit_id, msg_link, i, file_n):
                     file = path
                 thumb_path = "thumb.jpg"
                 
-                caption = f"{msg.caption}\n\n__Unrestricted by **[Team SPY](https://t.me/dev_gagan)**__" if msg.caption else "__Unrestricted by **[Team SPY](https://t.me/dev_gagan)**__"
+                caption = f"{msg.caption}\n\n__Unrestricted by **[🕸️𓆩𝐁𝐋𝐚𝐧𝐊𝐂𝐇𝐈𝐭𓆪🧸MvMahanta](https://t.me/neetbrandmedprep)**__" if msg.caption else "__Unrestricted by **[🕸️𓆩𝐁𝐋𝐚𝐧𝐊𝐂𝐇𝐈𝐭𓆪🧸MvMahanta](https://t.me/neetbrandmedprep)**__"
                 await send_document_with_chat_id(client, sender, path, caption, thumb_path, upm)
             os.remove(file)
             await upm.delete()
